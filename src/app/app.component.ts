af8025fb29e3f9e7051c92e6d428bfa970584d46
Jhonatan Plata <kaysinho@gmail.com>
2021-04-21 14:16:37 -0500

    Generacion de archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,6 +56,14 @@ export class AppComponent {
     reader.readAsBinaryString(target.files[0])
   }
 
+  reset() {
+    this.newRecords = []
+    this.deletedRecords = []
+    this.editedRecords = []
+    this.previousFile = []
+    this.newFile = []
+  }
+
   compare() {
 
     //new
@@ -67,7 +75,7 @@ export class AppComponent {
     console.log('news ', this.newRecords)
 
     //deleted
-    for (let old of this.previousFile){
+    for (let old of this.previousFile) {
       if (!this.newFile.some(l => l[4] === old[4])) {
         this.deletedRecords.push(old)
       }
@@ -75,12 +83,11 @@ export class AppComponent {
     console.log('deleted ', this.deletedRecords)
 
     //updated
-    for (let old of this.previousFile){
+    for (let old of this.previousFile) {
       for (let current of this.newFile) {
-        if (old[4] === current[4]){
-          if (!this.equals(old, current)){
+        if (old[4] === current[4]) {
+          if (!this.equals(old, current)) {
             this.editedRecords.push(current)
-
           }
         }
       }
@@ -90,8 +97,37 @@ export class AppComponent {
 
   }
 
-  equals  = (a:any, b:any): boolean => JSON.stringify(a) === JSON.stringify(b);
+  equals = (a: any, b: any): boolean => JSON.stringify(a) === JSON.stringify(b);
 
+  download(type: string) {
+
+    let fileName: string = '';
+    let file: Array<any> = [];
+    switch (type) {
+      case 'news':
+        fileName = 'new-records.xlsx';
+        file = this.newRecords;
+        break;
+      case 'deleted':
+        fileName = 'deleted-records.xlsx';
+        file = this.deletedRecords;
+        break;
+      case 'updated':
+        fileName = 'updated-records.xlsx';
+        file = this.editedRecords;
+        break;
+    }
+
+    /* table id is passed over here */
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(file);
+  
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, fileName);
+  }
 
 
 }
