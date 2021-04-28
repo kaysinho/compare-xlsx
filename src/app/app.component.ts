a2c2df412ce42599f1af14c03f96e1e89de72258
Jhonatan Plata <kaysinho@gmail.com>
2021-04-28 15:52:19 -0500

    Completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,69 +9,149 @@ import * as XLSX from 'xlsx';
 export class AppComponent {
   readingNewFile: boolean = false;
   readingPreviousFile: boolean = false;
-  writing: boolean = false;
+  writingNewFiles: boolean = false;
+  writingEditedFiles: boolean = false;
+  writingDeletedFiles: boolean = false;
   readonly NEW = 'new';
   readonly OLD = 'old';
   readonly startAt = 'A1';
+  columPositions : Array<number> = [];
+  readonly colums : Array<string> = ["Region", "Country", "WLC", "Location", "GUI", "UPN",
+  "Last Name", "First Name", "Service Line", "Organization", "SMU Name", "Title", "Rank", "Work Phone", "EA Name", "EA Phone"]
   newFile: any = [];
   previousFile: any = [];
   updateEveryMS = 1000;
   newRecords: Array<any> = [];
   deletedRecords: Array<any> = [];
   editedRecords: Array<any> = [];
-  constructor() {}
+  sheetName : string = "";
+  constructor() { }
+
   onFileChange(status: string, ev: any) {
-    status === this.NEW
-      ? (this.readingNewFile = true)
-      : (this.readingPreviousFile = true);
-    let workBook: any = null;
-    let jsonData = null;
+    if (status === this.NEW){
+      this.readingNewFile = true
+    }else{
+      this.readingPreviousFile = true;
+    }
+
+    let data = [];
     const reader = new FileReader();
     const file = ev.target.files[0];
-    reader.onload = (event) => {
-      const data = reader.result;
-      workBook = XLSX.read(data, { type: 'binary' });
-      const nameSheet = workBook.SheetNames[0]
-      const ref = workBook.Sheets[nameSheet]['!ref'];
-      const endAt = ref.split(':')[1];
-      const range = `${this.startAt}:${endAt}`;
-      jsonData = workBook.SheetNames.reduce((initial: any, name: any) => {
-        const sheet = workBook.Sheets[name];
-        initial[name] = XLSX.utils.sheet_to_json(sheet, { range });
-        return initial;
-      }, {});
+
+    reader.onload = (e:any) => {
+      console.log('Se carga el archivo')
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+      const wsname: string = wb.SheetNames[0];
+      this.sheetName = wsname;
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname]
+      data = (XLSX.utils.sheet_to_json(ws, { header: 1 }))
+
+      console.log('Archivo leido')
+      data = this.normalizeData(data)
+
+      console.log('Archivo procesado')
       if (status === this.NEW) {
-        this.newFile = jsonData;
+        this.newFile = data;
         this.readingNewFile = false;
       } else {
-        this.previousFile = jsonData;
+        this.previousFile = data;
         this.readingPreviousFile = false;
       }
+
     };
     reader.readAsBinaryString(file);
   }
 
+  normalizeData(data: any) {
+    let newData: Array<any> = [];
+
+    /** Delete first rows */
+    let i= 0;
+    let rowsFound = false;
+    while (!rowsFound) {
+      if (data[i].length>12){
+        rowsFound = true;
+      }else{
+        i++;
+      }
+    }
+    data = data.slice(i, data.length)
+    /** End delete first rows */
+
+    //It's for know the mandatory column positions
+    this.firstRow(data[0]);
+
+    //We create a new array only with the mandatory columns
+    let percentage = 0;
+    for (let i =0; i<data.length; i++){
+      let row = this.normalizeRow(data[i])
+      /*percentage = (((i+1) * 100) / data.length)
+      this.readingPercentagePreviousFile = percentage;
+      this.readingPercentageNewFile = percentage;
+      console.log(percentage)*/
+      if (row.length>0){
+        newData.push(row);
+      }
+    }
+
+    return newData;
+  }
+
+  firstRow(row:Array<any>){
+    this.columPositions = []
+   
+
+      for (let i=0; i<row.length; i++){
+        if (this.colums.includes(row[i])) {
+          this.columPositions.push(i)
+        };
+      }
+  }
+
+  normalizeRow(row:Array<any>):Array<any>{
+    let newRow : Array<any> =[]
+    for (let i=0; i<row.length; i++){
+      if (this.columPositions.includes(i)){
+        newRow.push(row[i])
+      }
+    }
+    return newRow;
+  }
+
+  
+
   async compare() {
     if (typeof Worker !== 'undefined') {
-      this.writing = true;
       const worker = new Worker('./excel.worker', { type: 'module' });
       worker.onmessage = (response) => {
         const {
-          data: { newRecords, editedRecords, deletedRecords },
+          data: { newRecords, editedRecords, deletedRecords, finished },
         } = response;
         if (newRecords) {
           this.newRecords = newRecords;
+          this.writingNewFiles = true
         }
-        if (editedRecords) {
-          this.editedRecords = editedRecords;
-        }
+
         if (deletedRecords) {
           this.deletedRecords = deletedRecords;
+          this.writingDeletedFiles = true
+          this.writingNewFiles = false
+        }
+
+        if (editedRecords) {
+          this.editedRecords = editedRecords;
+          this.writingDeletedFiles = false
+          this.writingEditedFiles = true
+        }
+
+        if(finished){
+          this.writingEditedFiles = false
         }
       };
       worker.postMessage({
-        newFile: this.newFile.Sheet1,
-        oldFile: this.previousFile.Sheet1,
+        newFile: this.newFile,
+        oldFile: this.previousFile,
       });
     } else {
       console.log('couldnt run');
@@ -97,12 +177,14 @@ export class AppComponent {
         break;
     }
 
+    file.unshift(this.colums)
+    console.log(file)
     /* table id is passed over here */
     const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(file);
-
+  
     /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.utils.book_append_sheet(wb, ws, this.sheetName);
 
     /* save to file */
     XLSX.writeFile(wb, fileName);
@@ -113,5 +195,11 @@ export class AppComponent {
     this.editedRecords = [];
     this.previousFile = [];
     this.newFile = [];
+
+    this.readingNewFile = false;
+    this.readingPreviousFile = false;
+    this.writingNewFiles = false;
+    this.writingEditedFiles = false;
+    this.writingDeletedFiles = false;
   }
 }
--- src/app/excel.worker.ts
+++ src/app/excel.worker.ts
@@ -6,20 +6,23 @@ addEventListener('message', ({ data }) => {
   let newRecords = [];
   let deletedRecords = [];
   let editedRecords = [];
-  const equals = (a: any, b: any): boolean =>
-    JSON.stringify(a) === JSON.stringify(b);
+  let finished : boolean = true;
+  const equals = (a: any, b: any): boolean => JSON.stringify(a) === JSON.stringify(b);
+
   //new
   for (let current of newFile) {
-    if (!oldFile.some((row: any) => row.GUI === current.GUI)) {
+    if (!oldFile.some((row: any) => row[4] === current[4])) {
       newRecords.push(current);
+      postMessage({ newRecords });
     }
   }
   postMessage({ newRecords });
 
   // deleted
   for (let old of oldFile) {
-    if (!newFile.some((row: any) => row.GUI === old.GUI)) {
+    if (!newFile.some((row: any) => row[4] === old[4])) {
       deletedRecords.push(old);
+      postMessage({ deletedRecords });
     }
   }
   postMessage({ deletedRecords });
@@ -27,12 +30,15 @@ addEventListener('message', ({ data }) => {
   //updated
   for (let old of oldFile) {
     for (let current of newFile) {
-      if (old.GUI === current.GUI) {
+      if (old[4] === current[4]) {
         if (!equals(old, current)) {
           editedRecords.push(current);
+          postMessage({ editedRecords });
         }
       }
     }
   }
   postMessage({ editedRecords });
+
+  postMessage({finished})
 });
